d44d68e75b9ba05978f5c47aacd8fec83db73160
MuhammadRezaHeidary <muhammadreza.heidary@gmail.com>
2023-06-29 18:01:31 +0330

    Initializing Project

---
+++ mekatron-random-expressions.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Mekatron Random Expressions
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Muhammmad Reza Heidary
+Author URI: http://URI_Of_The_Plugin_Author
+License: MIT
+*/
